d39ef0f9fbefa335a1f9db45bcbe82db7727a4a5
luisfalconmx <me@luisfalconmx.com>
2021-03-02 23:54:29 -0600

    feat: gulp-terser installed and configured for optimize js files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const pug = require('gulp-pug');
+const terser = require('gulp-terser')
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
@@ -22,7 +23,7 @@ gulp.task('compile:pug', () => {
     .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('compile:javascript', () => {
+gulp.task('compile:js', () => {
   return gulp
     .src('./src/javascript/**/*.js')
     .pipe(babel({ presets: ['@babel/preset-env'] }))
@@ -38,7 +39,7 @@ gulp.task('server', () => {
 
   gulp.watch('./src/styles/**/*.scss', gulp.series(['compile:sass'])).on('change', reload);
   gulp.watch('./src/pages/**/*.pug', gulp.series(['compile:pug'])).on('change', reload);
-  gulp.watch('./src/javascript/**/*.js', gulp.series(['compile:javascript'])).on('change', reload);
+  gulp.watch('./src/javascript/**/*.js', gulp.series(['compile:js'])).on('change', reload);
 });
 
 gulp.task('optimize:css', () => {
@@ -48,4 +49,11 @@ gulp.task('optimize:css', () => {
     .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('dev', gulp.series(['compile:sass', 'compile:pug', 'compile:javascript', 'server']));
+gulp.task('optimize:js', () => {
+  return gulp
+    .src('./dist/**/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('dev', gulp.series(['compile:sass', 'compile:pug', 'compile:js', 'server']));
