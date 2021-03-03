022bbafabe27a796966cdfc693ae74079ee798a2
luisfalconmx <me@luisfalconmx.com>
2021-03-02 21:18:02 -0600

    feat: gulp-pug installed and configured for compile pug files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
+
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const pug = require("gulp-pug");
 
 sass.compiler = require("node-sass");
 
-gulp.task("compile:sass", function () {
+gulp.task("compile:sass", () => {
   return gulp
     .src("./src/styles/index.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('compile:pug', () => {
+  return gulp
+    .src('./src/pages/**/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./dist"))
 });
