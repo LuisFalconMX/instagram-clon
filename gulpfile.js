b5f49e784b44d65a6191d56f35ccb274e895c7ce
luisfalconmx <me@luisfalconmx.com>
2021-03-02 23:51:30 -0600

    feat: gulp-clean-css has been installed and configured for optimize css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const pug = require("gulp-pug");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const pug = require('gulp-pug');
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
-sass.compiler = require("node-sass");
+sass.compiler = require('node-sass');
 
-gulp.task("compile:sass", () => {
+gulp.task('compile:sass', () => {
   return gulp
-    .src("./src/styles/index.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./dist"))
+    .src('./src/styles/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('compile:pug', () => {
   return gulp
     .src('./src/pages/**/*.pug')
     .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest("./dist"))
+    .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('compile:javascript', () => {
@@ -31,13 +32,20 @@ gulp.task('compile:javascript', () => {
 gulp.task('server', () => {
   browserSync.init({
     server: {
-      baseDir: "./dist/"
+      baseDir: './dist/'
     }
   });
 
-  gulp.watch("./src/styles/**/*.scss", gulp.series(['compile:sass'])).on("change", reload);
-  gulp.watch("./src/pages/**/*.pug", gulp.series(['compile:pug'])).on("change", reload);
-  gulp.watch("./src/javascript/**/*.js", gulp.series(['compile:javascript'])).on("change", reload);
+  gulp.watch('./src/styles/**/*.scss', gulp.series(['compile:sass'])).on('change', reload);
+  gulp.watch('./src/pages/**/*.pug', gulp.series(['compile:pug'])).on('change', reload);
+  gulp.watch('./src/javascript/**/*.js', gulp.series(['compile:javascript'])).on('change', reload);
 });
 
-gulp.task("dev", gulp.series(["compile:sass", "compile:pug", "compile:javascript", "server"]));
+gulp.task('optimize:css', () => {
+  return gulp
+    .src('./dist/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('dev', gulp.series(['compile:sass', 'compile:pug', 'compile:javascript', 'server']));
